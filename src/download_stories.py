85abb1742c056a841719884088ff448255f4ad05
Mike H <mike@brazen>
2012-08-11 14:57:11 +0100

    Added capability
    * Can now download comments
    * Now checks multiple (hard-coded) subreddits

--- src/download_stories.py
+++ src/download_stories.py
@@ -1,14 +1,92 @@
 import argparse
+import json
+import urllib
 from reddit.reddit_classes import *
 from datetime import *
+from reddit.reddit_client import RedditUrlOpener
+import time
+import os
+import logging
 
-parser = argparse.ArgumentParser(description='Download useful stories associated with a Reddit JSON file')
-parser.add_argument('file')
+REDDIT_URL='http://reddit.com'
+OUTPUT_DIR='../data/'
+RUN_DATE=datetime.utcnow().strftime("%Y%m%d")
+RUN_TIME=datetime.utcnow().strftime("%H%M%S")
 
-args = parser.parse_args()
+logging.basicConfig(level=logging.DEBUG)
+opener = RedditUrlOpener() # Custom URL opener with user-agent string
 
-file = args.file
+# Download a snapshot of a comment thread
+def download_snapshot(snapshot, attempt=0):
+	json_url =  REDDIT_URL + snapshot.permalink + '.json'
+	logging.debug("Opening " + json_url)
+	u = opener.open(json_url)
+	contents = u.read()
+	u.close()
+	if contents == '{"error": 304}':
+		if attempt == 0:
+			logging.info("Reddit has asked us to back off - sleeping for 30s")
+			time.sleep(30)
+			download_snapshot(snapshot,1)
+		else:
+			logging.error("Reddit has asked us to back off, we did, and it still won't give us the content. Moving on.")
+	else:
+		save_snapshot_comment(snapshot,contents)
+	logging.debug("Sleeping 5 seconds to respect Reddit API rules")
+	time.sleep(5)
 
-ss = SubredditSnapshot(datetime.now())
-ss.load_from_file(file)
+# Save the comment to the appropriate file
+def save_snapshot_comment(snapshot,json):
+	snapshot_dir = get_snapshot_folder(snapshot);
+	
+	filename = snapshot_dir + "/" + snapshot.id + ".json"
+	f = open(filename,'w')
+	f.write(json)
+	f.close()
+
+
+# Construct the folder structure and return a location
+def get_snapshot_folder(snapshot):
+	base_dir = OUTPUT_DIR + '/' + snapshot.subreddit
+	if not os.path.exists(base_dir):
+		os.makedirs(base_dir)
+
+	date_dir = base_dir + '/' + RUN_DATE
+	if not os.path.exists(date_dir):
+		os.makedirs(date_dir)		
+
+	snapshot_dir = date_dir + '/' + RUN_TIME
+	if not os.path.exists(snapshot_dir):
+		os.makedirs(snapshot_dir)	
+
+	return snapshot_dir
+
+# Main
+
+# CLI parser
+#parser = argparse.ArgumentParser(description='Download useful stories associated with a Reddit JSON file')
+#parser.add_argument('file')
+#args = parser.parse_args()
+#file = args.file
+
+# Construct object
+
+subreddits = {'programming','politics','malefashionadvice','atheism','unitedkingdom','apple','android','games','iphone'}
+
+for subreddit in subreddits:
+
+	ss = SubredditSnapshot(datetime.now())
+	url = REDDIT_URL + "/r/" + subreddit + "/.json"
+	logging.debug("Loading new subreddit from " + url)
+	ss.load_from_url(url)
+	logging.debug("Sleeping to respect Reddit API rules")
+	time.sleep(5) 
+
+	for snapshot in ss.stories:
+		if snapshot.num_comments > 0:
+			download_snapshot(snapshot)
+		
+
+
+
 
--- src/reddit/reddit_classes.py
+++ src/reddit/reddit_classes.py
@@ -1,4 +1,7 @@
 import json
+import logging
+from datetime import *
+from reddit.reddit_client import RedditUrlOpener
 
 class SubredditSnapshot(object):
 
@@ -6,6 +9,7 @@ class SubredditSnapshot(object):
 	def __init__(self, snapshot_time):
 		super(SubredditSnapshot, self).__init__()
 		self.snapshot_time = snapshot_time
+		self.stories = []
 
 	def snapshot_time(self):
 		return self.snapshot_time
@@ -13,25 +17,69 @@ class SubredditSnapshot(object):
 	def set_snapshot_time(self,snapshot_time_in):
 		self.snapshot_time = snapshot_time_in
 
+	def load_from_json(self,j):
+		children = j['data']['children']
+		for story in children:
+			story_snapshot = RedditStorySnapshot()
+			story_snapshot.load_from_dict(story['kind'],datetime.utcnow(),story['data'])
+			self.stories.append(story_snapshot)
+
 	def load_from_file(self,file):
 		f = open(file,'r')
 		j = json.load(f)
 		f.close()
-		print json.dumps(j, sort_keys=True, indent=4)
+		self.load_from_json(j)
+
+	def load_from_url(self, url):
+		opener = RedditUrlOpener() # Custom URL opener with user-agent string
+		u = opener.open(url)
+		j = json.load(u)
+		u.close()
+		self.load_from_json(j)
+
 
 
 
 	
 
 class RedditStorySnapshot(object):
+
 	"""A story (or link) on Reddit"""
-	def __init__(self, arg):
+	def __init__(self):
 		super(RedditStorySnapshot, self).__init__()
-		self.arg = arg
+		self.id = ""
+		self.kind = ""
+		self.domain = ""
+		self.subreddit = ""
+		self.title = ""
+		self.author = ""
+		self.created_utc = ""
+		self.snapshot_utc = ""
+		self.downs = ""
+		self.ups = ""
+		self.score = ""
+		self.subreddit_id = ""
+		self.url = ""
+		self.permalink = ""
+		self.num_comments = ""
 
-	def title(self):
-		return self.title
+	def load_from_dict(self, kind, snapshot_utc, dict):
+		self.kind = kind
+		self.id = dict['id']
+		self.domain = dict['domain']
+		self.subreddit = dict['subreddit']
+		self.title = dict['title']
+		self.author = dict['author']
+		self.created_utc = dict['created_utc']
+		self.snapshot_utc = snapshot_utc
+		self.downs = dict['downs']
+		self.ups = dict['ups']
+		self.score = dict['score']
+		self.subreddit_id = dict['subreddit_id']
+		self.url = dict['url']
+		self.permalink = dict['permalink']
+		self.num_comments = dict['num_comments']
 
-	def set_title(self,title_in):
-		self.title = title_in
+
+
 		
